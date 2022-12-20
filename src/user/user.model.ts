952307f6cbc651893580561b102501e0a31e96b0
Andrew Covenant <andrewcovenant@192-168-0-101.rdsnet.ro>
2022-12-20 23:04:37 +0200

    Validation for user implemented

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,9 +1,38 @@
 import * as mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v: string) => {
+        return v.length >= 3 && v.length <= 20;
+      },
+      message: (props) => `${props.value} is not a valid username!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v: string) => {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$/.test(
+          v,
+        );
+      },
+      message: (props) => `${props.value} is not a valid password!`,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v: string) => {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
 });
 
 export interface User {
